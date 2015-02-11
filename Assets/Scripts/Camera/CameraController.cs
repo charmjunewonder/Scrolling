c35068f0e9a1318231811915930a332a8a38be0a
EasternStar <feiranw@andrew.cmu.edu>
2015-02-11 01:36:21 -0500

    first scratch
    first scratch

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject character;
+
+
+	private float scrollingTimer;
+	private bool isZoomingOut;
+	private bool isZoomingIn;
+
+	// Use this for initialization
+	void Start () {
+		scrollingTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 pos = character.transform.position;
+		pos.z = transform.position.z;
+		transform.position = pos;
+
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0) 
+		{
+			isZoomingIn = true;
+		}
+
+		if(Input.GetAxis ("Mouse ScrollWheel") > 0)
+		{
+			isZoomingOut = true;
+		}
+
+		if (isZoomingOut) 
+		{
+			scrollingTimer += 0.02f;
+
+			transform.GetComponent<Camera>().orthographicSize += 0.02f;
+
+			if(scrollingTimer > 1)
+			{
+				isZoomingOut = false;
+
+				scrollingTimer = 0;
+			}
+		}
+		if (isZoomingIn) 
+		{
+			scrollingTimer += 0.02f;
+
+			transform.GetComponent<Camera>().orthographicSize -= 0.02f;
+
+			if(scrollingTimer > 1)
+			{
+				isZoomingIn = false;
+
+				scrollingTimer = 0;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			Vector3 pos = transform.position;
+			pos.x -= 0.2f;
+			transform.position = pos; 
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			Vector3 pos = transform.position;
+			pos.x += 0.2f;
+			transform.position = pos;
+			
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			Vector3 pos = transform.position;
+			pos.y += 0.2f;
+			transform.position = pos;
+			
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			Vector3 pos = transform.position;
+			pos.y -= 0.2f;
+			transform.position = pos;
+			
+		}
+	}
+}
