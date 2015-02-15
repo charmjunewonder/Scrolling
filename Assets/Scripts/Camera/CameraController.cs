00c3fb8ecfd539679a6a184e3773c4e21dc8cc76
EasternStar <feiranw@andrew.cmu.edu>
2015-02-15 04:36:22 -0500

    Eric! finish the camera shit!!!!!

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -12,8 +12,11 @@ public class CameraController : MonoBehaviour {
 	private Vector3 previousCharacterPos;
 
     private Vector3 targetPosition;
+
+    private bool scrollingLock;
 	// Use this for initialization
 	void Start () {
+        scrollingLock = false;
 		scrollingTimer = 0;
 		previousCharacterPos = Vector3.zero;
 	}
@@ -30,58 +33,62 @@ public class CameraController : MonoBehaviour {
 			transform.position = pos;
 		}
 
-        if (Mathf.Abs(character.transform.position.y - transform.position.y) > cameraSize  * 0.6f)
+        if (Mathf.Abs(character.transform.position.y - transform.position.y) > cameraSize  * 0.8f)
         {
             Vector3 pos = transform.position;
             pos.y += character.transform.position.y - previousCharacterPos.y;
             transform.position = pos;
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") < 0 && transform.GetComponent<Camera>().orthographicSize > 10) 
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && transform.GetComponent<Camera>().orthographicSize > 10 && !scrollingLock) 
 		{
 			isZoomingIn = true;
+            scrollingLock = true;
+            targetPosition = (character.transform.position - transform.position) * 3 / cameraSize;
             
-            Debug.Log(targetPosition);
 		}
 
-        if (Input.GetAxis("Mouse ScrollWheel") > 0 && transform.GetComponent<Camera>().orthographicSize < 50)
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && transform.GetComponent<Camera>().orthographicSize < 50 && !scrollingLock)
 		{
 			isZoomingOut = true;
+            scrollingLock = true;
+            targetPosition = -(character.transform.position - transform.position) * 3 / cameraSize;
             
-            Debug.Log(targetPosition);
 		}
 
-        if (isZoomingOut && transform.GetComponent<Camera>().orthographicSize < 50) 
-		{
+        if (isZoomingOut && transform.GetComponent<Camera>().orthographicSize < 50)
+        {
+            Debug.Log(targetPosition);
 			scrollingTimer += 0.2f;
 
 			transform.GetComponent<Camera>().orthographicSize += 0.5f;
 
             character.GetComponent<CharacterController>().changeSize(0.01f);
-            targetPosition = -(character.transform.position - transform.position) * 3 / cameraSize;
+            
             transform.position += new Vector3(targetPosition.x, targetPosition.y, 0) / 6;
 
-			if(scrollingTimer > 1)
+            if (scrollingTimer > 1 || transform.GetComponent<Camera>().orthographicSize >= 50)
 			{
 				isZoomingOut = false;
-
+                scrollingLock = false;
 				scrollingTimer = 0;
 			}
 		}
-        if (isZoomingIn && transform.GetComponent<Camera>().orthographicSize > 10) 
-		{
+        if (isZoomingIn && transform.GetComponent<Camera>().orthographicSize > 10)
+        {
+            Debug.Log(targetPosition);
 			scrollingTimer += 0.2f;
             
 			transform.GetComponent<Camera>().orthographicSize -= 0.5f;
-            targetPosition = (character.transform.position - transform.position) * 3 / cameraSize;
+            
             character.GetComponent<CharacterController>().changeSize(-0.01f);
 
             transform.position += new Vector3(targetPosition.x, targetPosition.y, 0) / 6;
 
-			if(scrollingTimer > 1)
+            if (scrollingTimer > 1 || transform.GetComponent<Camera>().orthographicSize <= 10)
 			{
 				isZoomingIn = false;
-
+                scrollingLock = false;
 				scrollingTimer = 0;
 			}
 		}
