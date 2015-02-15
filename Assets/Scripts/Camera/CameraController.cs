f16d14dbc109b08291a8f982a9d4eef8bdd2f625
EasternStar <feiranw@andrew.cmu.edu>
2015-02-15 04:22:31 -0500

    Valentine's tomorrow

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -11,6 +11,7 @@ public class CameraController : MonoBehaviour {
 	private bool isZoomingIn;
 	private Vector3 previousCharacterPos;
 
+    private Vector3 targetPosition;
 	// Use this for initialization
 	void Start () {
 		scrollingTimer = 0;
@@ -24,29 +25,41 @@ public class CameraController : MonoBehaviour {
 		cameraSize = transform.GetComponent<Camera>().orthographicSize;
 
 		if (Mathf.Abs(character.transform.position.x - transform.position.x) > cameraSize * 2 * 0.5f) {
-			Debug.Log("fjskl");
 			Vector3 pos = transform.position;
 			pos.x += character.transform.position.x - previousCharacterPos.x;
 			transform.position = pos;
 		}
 
-		if (Input.GetAxis ("Mouse ScrollWheel") < 0) 
+        if (Mathf.Abs(character.transform.position.y - transform.position.y) > cameraSize  * 0.6f)
+        {
+            Vector3 pos = transform.position;
+            pos.y += character.transform.position.y - previousCharacterPos.y;
+            transform.position = pos;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && transform.GetComponent<Camera>().orthographicSize > 10) 
 		{
 			isZoomingIn = true;
+            
+            Debug.Log(targetPosition);
 		}
 
-		if(Input.GetAxis ("Mouse ScrollWheel") > 0)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && transform.GetComponent<Camera>().orthographicSize < 50)
 		{
 			isZoomingOut = true;
+            
+            Debug.Log(targetPosition);
 		}
 
-		if (isZoomingOut) 
+        if (isZoomingOut && transform.GetComponent<Camera>().orthographicSize < 50) 
 		{
 			scrollingTimer += 0.2f;
 
-			transform.GetComponent<Camera>().orthographicSize += 0.2f;
+			transform.GetComponent<Camera>().orthographicSize += 0.5f;
 
-			character.GetComponent<CharacterController>().changeSize(0.004f);
+            character.GetComponent<CharacterController>().changeSize(0.01f);
+            targetPosition = -(character.transform.position - transform.position) * 3 / cameraSize;
+            transform.position += new Vector3(targetPosition.x, targetPosition.y, 0) / 6;
 
 			if(scrollingTimer > 1)
 			{
@@ -55,12 +68,15 @@ public class CameraController : MonoBehaviour {
 				scrollingTimer = 0;
 			}
 		}
-		if (isZoomingIn) 
+        if (isZoomingIn && transform.GetComponent<Camera>().orthographicSize > 10) 
 		{
 			scrollingTimer += 0.2f;
+            
+			transform.GetComponent<Camera>().orthographicSize -= 0.5f;
+            targetPosition = (character.transform.position - transform.position) * 3 / cameraSize;
+            character.GetComponent<CharacterController>().changeSize(-0.01f);
 
-			transform.GetComponent<Camera>().orthographicSize -= 0.2f;
-			character.GetComponent<CharacterController>().changeSize(-0.004f);
+            transform.position += new Vector3(targetPosition.x, targetPosition.y, 0) / 6;
 
 			if(scrollingTimer > 1)
 			{
--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -3,43 +3,37 @@ using System.Collections;
 
 public class CharacterController : MonoBehaviour {
 
+    private GameObject camera;
 	// Use this for initialization
 	void Start () {
-	
+        camera = GameObject.Find("Main Camera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.A)) {
-			Vector3 pos = transform.position;
-			pos.x -= 0.2f;
-			transform.position = pos; 
+			/*Vector3 pos = transform.position;
+            pos.x -= 0.5f * camera.GetComponent<Camera>().orthographicSize / 50;
+			transform.position = pos;*/
+            transform.rigidbody2D.velocity = new Vector3(-30 * camera.GetComponent<Camera>().orthographicSize / 50,  transform.rigidbody2D.velocity.y , 0);
 		}
 
 		if (Input.GetKey (KeyCode.D)) {
-			Vector3 pos = transform.position;
-			pos.x += 0.2f;
+			/*Vector3 pos = transform.position;
+            pos.x += 0.5f * camera.GetComponent<Camera>().orthographicSize / 50;
 			transform.position = pos;
-			
-		}
-
-		if (Input.GetKey (KeyCode.W)) {
-			Vector3 pos = transform.position;
-			pos.y += 0.2f;
-			transform.position = pos;
-			
-		}
-
-		if (Input.GetKey (KeyCode.S)) {
-			Vector3 pos = transform.position;
-			pos.y -= 0.2f;
-			transform.position = pos;
+			*/
+            transform.rigidbody2D.velocity = new Vector3(30 * camera.GetComponent<Camera>().orthographicSize / 50, transform.rigidbody2D.velocity.y, 0);
 		}
 
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.rigidbody2D.velocity = new Vector3(0, 50, 0);
+        }
 	}
 
 	public void changeSize(float increment){
-		transform.localScale += new Vector3(increment, increment*0.5f, 0);
+		transform.localScale += new Vector3(increment, increment, 0);
 	}
 }
